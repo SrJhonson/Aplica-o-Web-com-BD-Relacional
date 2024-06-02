8c495aefaf594a80f021a02b7a692a9cc1bb94a1
Jhonatan Dias Farias <diasjhonatan24@gmail.com>
2024-06-02 18:19:12 -0300

    porta 3000

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const path = require('path');
-const sql = require('mssql');
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const cors = require('cors');
+
 
 const app = express();
+app.use(cors());
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Configuração do banco de dados
 const config = {
